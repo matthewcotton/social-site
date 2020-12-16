af3f34901a60b5bcda0158b970ddf200985adecf
matthewcotton <matthewneilcotton@gmail.com>
2020-12-16 17:21:52 +0000

    add likes function and rebranded site

--- src/Add.js
+++ src/Add.js
@@ -1,19 +1,26 @@
 import React from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-//import toastr from 'toastr';
-//import 'toastr/build/toastr.min.css';
+import toastr from 'toastr';
+import 'toastr/build/toastr.min.css';
 
 class Add extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       id: 0,
-      ref: React.createRef(),
+      ref: "",
       username: "",
       text: "",
       likes: 0,
     };
+    toastr.options = {
+      "closeButton": "true",
+      "positionClass": "toast-top-center",
+      "newestOnTop": "true",
+      "timeOut": "3000",
+    };
+    toastr.clear();
   }
 
   handleChange(event) {
@@ -24,17 +31,19 @@ class Add extends React.Component {
 
   submitHandler(event) {
     event.preventDefault();
+    const newID = this.props.postListLength + 1;
+    const newRef = React.createRef();
     this.props.onsubmit(
-      this.state.id,
-      this.state.ref,
+      newID,
+      newRef,
       this.state.username,
       this.state.text,
-      this.state.likes
+      0
     );
-    //toastr.success("Posted");
+    toastr.success("Buck up! Your post was successfully published.", "Published posted");
     this.setState((state) => ({
-      id: state.id + 1,
-      ref: state.ref + 1,
+      id: newID,
+      ref: "",
       username: "",
       text: "",
       likes: 0,
@@ -44,7 +53,6 @@ class Add extends React.Component {
   render() {
     return (
       <Form
-        onChange={(e) => this.handleChange(e)}
         onSubmit={(e) => this.submitHandler(e)}
       >
         <Form.Group controlId="postUsername">
@@ -53,6 +61,8 @@ class Add extends React.Component {
             name="username"
             type="text"
             value={this.state.username}
+            placeholder="enter username"
+            onChange={(e) => this.handleChange(e)}
           />
         </Form.Group>
 
@@ -62,6 +72,8 @@ class Add extends React.Component {
             name="text"
             type="text"
             value={this.state.text}
+            placeholder="add text"
+            onChange={(e) => this.handleChange(e)}
           />
         </Form.Group>
 
--- src/MyNav.js
+++ src/MyNav.js
@@ -2,7 +2,7 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import { HashRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Link from "react-router-dom/Link";
 import "./App.css";
 
 class MyNav extends React.Component {
@@ -10,14 +10,14 @@ class MyNav extends React.Component {
   render() {
     return (
       <Navbar bg="dark" variant="dark" expand="md">
-          <Navbar.Brand>MyFace</Navbar.Brand>
+          <Link to="/"><Navbar.Brand>FaceBuck</Navbar.Brand></Link>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
-              <Navbar.Text>
+              <Navbar.Text className="nav-link">
                 <Link to="/">View Posts</Link>
               </Navbar.Text>
-              <Navbar.Text>
+              <Navbar.Text className="nav-link">
                 <Link to="/add">Add Posts</Link>
               </Navbar.Text>
             </Nav>
